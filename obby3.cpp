38046a89b09367a022d0155df1e310b3b3ce16a0
Deeh878 <dainesskato@gmail.com>
2025-01-10 16:09:14 +0300

    Add files via upload

---
+++ obby3.cpp
@@ -0,0 +1,8 @@
+    std::cout << "Enter the second number: ";
+    std::cin >> num2;
+
+    // Perform arithmetic operations
+    double sum = num1 + num2;
+    double difference = num1 - num2;
+    double product = num1 * num2;
+    double quotient = num1 / num2;
